c9f1122c176d3d5897086dd402f4a91e5c65bb32
William <william310391@gmail.com>
2022-08-29 16:42:27 -0500

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {pool} from '../../config/db'
+export default async function handler(req, res) {
+  
+  const [rows] = await pool.query('SELECT NOW()');
+  res.status(200).json({ HORA_SERVER: rows[0]['NOW()'] })
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  
 }
+
